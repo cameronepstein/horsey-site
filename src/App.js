ba4a356461ab7631085f2b74dbe35de1e4d80aea
TheoMcCabe <theomccabe@mac.com>
2019-02-05 12:28:31 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactPlayer from 'react-player';
+import logo from './Horsey_Text5.png';
+import styled from 'styled-components';
+
+import { FaFacebookSquare, FaInstagram, FaSpotify, FaShoppingCart, FaYoutube } from 'react-icons/fa';
+
 import './App.css';
+
+const LogoImage = styled.img`
+width: 500px;
+
+@media only screen and (max-width: 640px) {
+  width: 400px;
+}
+
+@media only screen and (max-width: 480px) {
+  width: 320px;
+}
+
+@media only screen and (max-width: 400px) {
+  width: 280px;
+}
+`;
+
+const SocialsList = styled.ul`
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+`;
+
+const SocialsElement = styled.li`
+  float: left;
+
+  a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    text-decoration: none;
+
+    :hover {
+      color: #000000;
+    }
+  };
+
+`;
+
+const StyledPlayer = styled(ReactPlayer)`
+margin: 50px 0;
+
+@media only screen and (max-width: 640px) {
+  width: 480px !important;
+  height: 270px !important;
+}
+
+  @media only screen and (max-width: 480px) {
+    width: 400px !important;
+    height: 225px !important;
+  }
+  
+  @media only screen and (max-width: 400px) {
+    width: 320px !important;
+    height: 180px !important;
+  }
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        {/* <header className="App-header">
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </header> */}
+
+
+        <body className={"App-body"}>
+        <LogoImage  src={logo}/>
+
+
+        <StyledPlayer url='https://www.youtube.com/watch?v=1Boc5wg-waY' playing />
+
+
+        <div >
+          <SocialsList >
+          <SocialsElement hoverColor={'#3b5998'}><a href="https://www.facebook.com/horseyband" target="_blank"><FaFacebookSquare/></a></SocialsElement>
+            <SocialsElement hoverColor={'#e95950'}><a href="https://www.instagram.com/horseyband/" target="_blank"><FaInstagram/></a></SocialsElement>
+          <SocialsElement hoverColor={'#3b5998'}><a href="https://www.youtube.com/channel/UCpYJKeL-jFzra-F-6TODnaA?view_as=subscriber" target="_blank"><FaYoutube/></a></SocialsElement>
+            <SocialsElement hoverColor={'#1DB954'}><a href="https://open.spotify.com/artist/3ey9OSASkq8HNPh0u7jjfO" target="_blank"><FaSpotify/></a></SocialsElement>
+            <SocialsElement hoverColor={'#1DB954'}><a href="https://horsey.bigcartel.com/" target="_blank"><FaShoppingCart/></a></SocialsElement>
+          </SocialsList>
+        </div>
+        </body>
       </div>
     );
   }
